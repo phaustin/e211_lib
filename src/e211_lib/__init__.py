467885b5ad7c8a3a680bf35d580a39c55a4fb59b
Phil Austin <paustin@eos.ubc.ca>
2021-09-26 09:39:49 -0700

    working with MANIFEST.in

--- src/e211_lib/__init__.py
+++ src/e211_lib/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 root_dir = Path(__file__).resolve().parent
 print(f"{root_dir=}")
-version_file= root_dir / 'VERSION.txt'
+version_file= root_dir / 'VERSION'
 
 if not version_file.is_file():
     __version__ = 'no_version'
--- write_version.py
+++ write_version.py
@@ -5,7 +5,7 @@ print(f"{root_dir=}")
 code_dir= root_dir / 'src/e211_lib'
 git_version = get_version(root=str(root_dir))
 print(f"{git_version=}")
-version_file = code_dir / 'VERSION.txt'
+version_file = code_dir / 'VERSION'
 with open(version_file,'w') as outfile:
     outfile.write(git_version)
 print((f"wrote new version number {git_version=}\n"
