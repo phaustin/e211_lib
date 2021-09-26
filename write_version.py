01d3d08bbc632589376c35f56ae55c271c8dcca2
Phil Austin <paustin@eos.ubc.ca>
2021-09-26 09:18:36 -0700

    working with write_version

--- write_version.py
+++ write_version.py
@@ -1,10 +1,11 @@
 from setuptools_scm import get_version
 from pathlib import Path
 root_dir = Path().resolve()
+print(f"{root_dir=}")
 code_dir= root_dir / 'src/e211_lib'
 git_version = get_version(root=str(root_dir))
 print(f"{git_version=}")
-version_file = code_dir / 'VERSΙΟΝ.txt'
+version_file = code_dir / 'VERSION.txt'
 with open(version_file,'w') as outfile:
     outfile.write(git_version)
 print((f"wrote new version number {git_version=}\n"
